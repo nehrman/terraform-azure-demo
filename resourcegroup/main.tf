6a51e9501e6a83255c0bfb187146441cbde58335
nehrman <nicolas.ehrman@gmail.com>
2019-01-17 20:54:52 +0100

    ...

--- resourcegroup/main.tf
+++ resourcegroup/main.tf
@@ -1,13 +1,13 @@
 module "rg" {
   source                     = "app.terraform.io/Hashicorp-neh-Demo/rg/azure"
   version                    = "0.1"
-//   tf_az_name                 = ""
-//   tf_az_env                  = ""
-//   tf_az_location             = ""
-//   tf_az_net_addr_space       = ""
-//   tf_az_subnet_prefixes      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-//   tf_az_subnet_names         = ["subnet1", "subnet2", "subnet3"]
-//   tf_az_storage_account_name = ""
+  tf_az_name                 = "${az_name}"
+  tf_az_env                  = "${az_env}"
+  tf_az_location             = "${az_location}"
+  tf_az_net_addr_space       = "${az_net_addr_space}"
+  tf_az_subnet_prefixes      = "${az_subnet_prefixes}"
+  tf_az_subnet_names         = "${az_subnet_names }"
+  tf_az_storage_account_name = "${az_storage_account_name}"
 
   tf_az_tags = {
     environment = "dev"
--- resourcegroup/variables.tf
+++ resourcegroup/variables.tf
@@ -13,3 +13,23 @@ variable client_secret {
 variable tenant_id {
   description = "Azure Tenant ID"
 }
+
+variable az_name {
+    description = "Name"
+}
+
+variable az_env {}
+
+variable az_location {}
+
+variable az_net_addr_space {}
+
+variable az_subnet_prefixes {
+    type = "list"
+}
+
+variable az_subnet_names {
+    type = "list"
+}
+
+variable az_storage_account_name {}
