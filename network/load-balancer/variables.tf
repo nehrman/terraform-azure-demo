ae50dcf9fa45c47b229f1ad5fc63c1507f0e6381
nehrman <nicolas.ehrman@gmail.com>
2019-01-17 22:24:56 +0100

    Reconfiguring config to use private registry

--- network/load-balancer/variables.tf
+++ network/load-balancer/variables.tf
@@ -1,1 +1,20 @@
+variable az_name {
+  description = "Name"
+}
 
+variable az_env {}
+
+variable az_location {}
+
+variable az_ft_name {}
+
+variable az_lb_probes_port {}
+
+variable az_lb_nb_probes {}
+
+variable az_lb_rule_proto {}
+
+variable az_lb_rule_ft_port {}
+
+variable az_lb_rule_bck_port {}
+s
