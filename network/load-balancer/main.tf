c4d947adddc1b2b2b995f0d631e80d8303f94b99
nehrman <nicolas.ehrman@gmail.com>
2019-01-17 22:51:50 +0100

    Correctly defined, i hope, the name of the variable.

--- network/load-balancer/main.tf
+++ network/load-balancer/main.tf
@@ -18,7 +18,7 @@ module "lb" {
   tf_az_lb_rule_proto    = "${var.az_lb_rule_proto }"
   tf_az_lb_rule_ft_port  = "${var.az_lb_rule_ft_port }"
   tf_az_lb_rule_bck_port = "${var.az_lb_rule_bck_port}"
-  tf_az_rg_name          = "${data.terraform_remote_state.rg.name}"
+  tf_az_rg_name          = "${data.terraform_remote_state.rg.resource_group_ name}"
 
   tf_az_tags = "${var.az_tags}"
 }
