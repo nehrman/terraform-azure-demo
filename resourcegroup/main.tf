29032540926d9d16cfff04ac2ce3e5c377fcf824
nehrman <nicolas.ehrman@gmail.com>
2019-01-18 00:11:27 +0100

    ...

--- resourcegroup/main.tf
+++ resourcegroup/main.tf
@@ -12,6 +12,10 @@ module "rg" {
   tf_az_tags = "${var.az_tags}"
 }
 
+output "resource_group_id" {
+  value = "${module.rg.resource_group_id}"
+}
+
 output "resource_group_name" {
   value = "${module.rg.resource_group_name}"
 }
