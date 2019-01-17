82abcb2dcab125dcfedc8b61d3f4eb3884d4da5a
nehrman <nicolas.ehrman@gmail.com>
2019-01-18 00:17:39 +0100

    Should be fine now

--- network/load-balancer/main.tf
+++ network/load-balancer/main.tf
@@ -2,7 +2,7 @@ data "terraform_remote_state" "rg" {
   backend = "atlas"
 
   config {
-    name = "Hashicorp-neh-Demo/webapp1-resourcegroup-dev"
+    name = "Hashicorp-neh-Demo/webapp1-resourcegroup-${var.az_env}"
   }
 }
 
