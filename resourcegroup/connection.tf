f9ee4d0d553622462824333a3dcd394af5091362
nehrman <nicolas.ehrman@gmail.com>
2019-01-17 20:35:19 +0100

    test

--- resourcegroup/connection.tf
+++ resourcegroup/connection.tf
@@ -5,7 +5,3 @@ provider "azurerm" {
   tenant_id       = "${var.tenant_id}"
 }
 
-variable subscription_id {}
-variable client_id {}
-variable client_secret {}
-variable tenant_id {}
--- resourcegroup/main.tf
+++ resourcegroup/main.tf
@@ -14,21 +14,6 @@ module "rg" {
   }
 }
 
-// module "azurerg" {
-//   source                     = "../../modules/azure-rg"
-//   tf_az_name                 = "test"
-//   tf_az_env                  = "staging"
-//   tf_az_location             = "westeurope"
-//   tf_az_net_addr_space       = "10.0.0.0/16"
-//   tf_az_subnet_prefixes      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-//   tf_az_subnet_names         = ["subnet1", "subnet2", "subnet3"]
-//   tf_az_storage_account_name = "stagingtestsa"
-
-//   tf_az_tags = {
-//     environment = "staging"
-//   }
-// }
-
 output "resource_group_name" {
   value = "${module.rg.resource_group_name}"
 }
