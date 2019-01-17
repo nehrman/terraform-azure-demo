cd97158792681f473c045b37f52f9f3b7c4b88d9
nehrman <nicolas.ehrman@gmail.com>
2019-01-17 20:15:42 +0100

    ...

--- resourcegroup/main.tf
+++ resourcegroup/main.tf
@@ -21,13 +21,13 @@ module "rg" {
 // }
 
 output "resource_group_name" {
-    value = "${module.azurerg.resource_group_name}"
+    value = "${module.rg.resource_group_name}"
 }
 
 output "virtual_network_name" {
-    value = "${module.azurerg.virtual_network_name}"
+    value = "${module.rg.virtual_network_name}"
 }
 
 output "subnets_name" {
-    value = "${module.azurerg.virtual_network_subnets}"
+    value = "${module.rg.virtual_network_subnets}"
 }
