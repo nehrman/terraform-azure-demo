a52b5034fefd6ce2c8c29c88bf86bd2aca196704
nehrman <nicolas.ehrman@gmail.com>
2019-01-17 20:22:11 +0100

    ...

--- resourcegroup/main.tf
+++ resourcegroup/main.tf
@@ -3,6 +3,16 @@
 module "rg" {
   source  = "app.terraform.io/Hashicorp-neh-Demo/rg/azure"
   version = "0.1"
+  tf_az_name                 = "test"
+  tf_az_env                  = "staging"
+  tf_az_location             = "westeurope"
+  tf_az_net_addr_space       = "10.0.0.0/16"
+  tf_az_subnet_prefixes      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  tf_az_subnet_names         = ["subnet1", "subnet2", "subnet3"]
+  tf_az_storage_account_name = "stagingtestsa"
+  tf_az_tags = {
+    environment = "staging"
+  }
 }
 
 // module "azurerg" {
