b126f614e4a1536aa481dd0b082a89218d4c9df9
nehrman <nicolas.ehrman@gmail.com>
2019-01-18 09:19:13 +0100

    Module Version

--- services/web-server/main.tf
+++ services/web-server/main.tf
@@ -1,6 +1,6 @@
 module "instance" {
   source              = "app.terraform.io/Hashicorp-neh-Demo/instance/azure"
-  version             = "1.0.1"
+  version             = "1.0.2"
   tf_az_name          = "${var.az_name}"
   tf_az_env           = "${var.az_env}"
   tf_az_location      = "${var.az_location}"
