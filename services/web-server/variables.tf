5946e45129b08311bff207a25abe9c74825d97b2
nehrman <nicolas.ehrman@gmail.com>
2019-06-26 20:02:59 +0200

    Adding Default Value to some variables

--- services/web-server/variables.tf
+++ services/web-server/variables.tf
@@ -7,7 +7,9 @@ variable az_location {
   description = "Define the Azure Region to deploy the resources"
   default = "francecentral"
 }
-variable az_nb_instance {}
+variable az_nb_instance {
+  default = 2
+}
 variable az_prefix {}
 variable az_instance_type {}
 
