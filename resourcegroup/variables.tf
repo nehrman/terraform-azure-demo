4cbd30a37dc10a7ccab3918d318e0d4e07ff7f02
nehrman <nicolas.ehrman@gmail.com>
2019-06-26 19:57:23 +0200

    Adding Default value to some variable

--- resourcegroup/variables.tf
+++ resourcegroup/variables.tf
@@ -11,20 +11,25 @@ variable az_location {
   default = "francecentral"
 }
 
-variable az_net_addr_space {}
+variable az_net_addr_space {
+   description = "Define, via a list, the subnets prefixes. Must be in correlation with number or subnets"
+   default = "10.0.0.0/8"
+}
 
 variable az_subnet_prefixes {
   description = "Define, via a list, the subnets prefixes. Must be in correlation with number or subnets"
   type = "list"
+  default = ["10.0.1.0/24","10.0.2.0/24"]
 }
 
 variable az_subnet_names {
   description = "Define, via a list, the name of subnets you wan to create"
   type = "list"
+  default = ["subnet1","subnet2"]
 }
 
 variable az_storage_account_name {
-  description = "DEfine the name of the Storage Account. Must e lowercase"
+  description = "Define the name of the Storage Account. Must e lowercase"
 }
 
 variable az_tags {
