af59da3afa91ed058c8e0d678eed1c1670ba77fc
nehrman <nicolas.ehrman@gmail.com>
2019-07-25 17:28:11 +0200

    Data sources

--- network/load-balancer/data.tf
+++ network/load-balancer/data.tf
@@ -2,7 +2,7 @@ data "terraform_remote_state" "rg" {
   backend = "remote"
 
   config = {
-    organization = "Hahsicorp-neh-Demo"
+    organization = "Hashicorp-neh-Demo"
 
     workspaces = {
       name = "Hashicorp-neh-Demo/arm-resourcegroup-${var.az_env}"
