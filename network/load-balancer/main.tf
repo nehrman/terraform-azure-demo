52495cf01afbc5ad812ababa10ded838a50883d0
nehrman <nicolas.ehrman@gmail.com>
2019-07-31 13:30:20 +0200

    Updating Version of the module

--- network/load-balancer/main.tf
+++ network/load-balancer/main.tf
@@ -1,6 +1,6 @@
 module "lb" {
   source                   = "app.terraform.io/Hashicorp-neh-Demo/lb/azurerm"
-  version                  = "1.0.5"
+  version                  = "1.0.6"
   tf_az_name               = "${var.az_name}"
   tf_az_env                = "${var.az_env}"
   tf_az_location           = "${var.az_location}"
